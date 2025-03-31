68762d1085decc200f1d31abdae937c65a794c16
HeatherKates <hkates@ufl.edu>
2025-03-31 16:02:55 -0400

    updated sif file ref

--- test_rnaseqreport.bash
+++ test_rnaseqreport.bash
@@ -2,8 +2,8 @@ module load nextflow
 module load apptainer
 
 nextflow run test_rmarkdown.nf \
-  -profile standard \
-  --sample_id CX_lung \
-  --params_file /blue/cancercenter-dept/hkates/RNAseq_reporting/CX_lung_report_params.txt \
-  --rmarkdown_container /blue/cancercenter-dept/hkates/RNAseq_reporting/rmarkdown_report/rmarkdown_report.sif \
+  -profile singularity \
+  --sample_id CX_lung_test_contrast \
+  --params_file CX_lung_report_params.txt  \
+  --rmarkdown_container /blue/cancercenter-dept/PIPELINES/rmarkdown_report_v2.sif \
   --rmarkdown_outdir results/reports
